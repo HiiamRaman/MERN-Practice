42a742bd98f3ef552d8c481e48f1f21e74128216
Raman Singh <ramansinghchauhan787@gmail.com>
2026-06-10 17:26:30 +0545

    tailwind practice

---
+++ TailwindwithReact/Tailwind-practice/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import ProfileCard from './Components/ProfileCard'
+function App (){
+
+    return (<div>
+   <ProfileCard/>
+  </div>)
+  
+}
+export default App
